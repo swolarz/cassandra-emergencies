d798e4cf576fca78fad4ddef78ec7897d8cec535
swolarz <szymon.wol@vp.pl>
2020-01-19 03:24:21 +0100

    Specyfikacja clustra cassandry dla docker swarma Skrypt startujacy cluster

---
+++ orchestration/cluster-startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Creating new network for service..."
+docker network create --driver overlay --scope swarm cassandra-net
+
+echo "Deploying emergencies service stack..."
+docker stack deploy -c emergencies-stack.yaml emergencies-cluster
